e06ca130dee89b1383e3d0c1470d149776c37022
Herbert von Broeuschmeul <Herbert.Broeuschmeul@gmail.com>
2011-01-14 23:25:24 +0100

    add copyright and copying notice in SirfUtils.java

--- src/org/broeuschmeul/android/gps/sirf/util/SirfUtils.java
+++ src/org/broeuschmeul/android/gps/sirf/util/SirfUtils.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2010 Herbert von Broeuschmeul
+ * Copyright (C) 2010 BluetoothGPS4Droid Project
+ * 
+ * This file is part of BluetoothGPS4Droid.
+ *
+ * BluetoothGPS4Droid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * BluetoothGPS4Droid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with BluetoothGPS4Droid. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.broeuschmeul.android.gps.sirf.util;
 
 import java.math.BigInteger;
