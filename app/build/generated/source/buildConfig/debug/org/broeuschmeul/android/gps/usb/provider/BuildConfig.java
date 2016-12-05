a66ab4e453d9a130e51d2b4283ac6ae6ed740064
Oliver Bell <freshollie@gmail.com>
2016-12-05 23:06:17 +0000

    Fixed all marshmallow issues, ready to commit to master

--- app/build/generated/source/buildConfig/debug/org/broeuschmeul/android/gps/usb/provider/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/org/broeuschmeul/android/gps/usb/provider/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "org.broeuschmeul.android.gps.usb.provider";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 127;
+  public static final int VERSION_CODE = 150;
   public static final String VERSION_NAME = "@string/versionName";
 }
